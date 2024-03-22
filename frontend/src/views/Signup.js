0faf62e0f0a896994f327011ddba0ba275a1fbfe
NarayanSapkota <59175636+noxiousghost@users.noreply.github.com>
2024-03-22 22:13:13 +0545

    added country selectro dropdown

--- frontend/src/views/Signup.js
+++ frontend/src/views/Signup.js
@@ -1,25 +1,86 @@
-import { Formik } from "formik";
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { Formik } from "formik";
+import Select from "react-select";
+import countries from "i18n-iso-countries";
+import englishCountries from "i18n-iso-countries/langs/en.json";
+import emojiFlag from "emoji-flag";
+
 import TextField from "../Components/TextField";
 import { signupValidationSchema } from "../FormValidation/validationSchema";
 import { signup } from "../services/loginSignup";
 
+countries.registerLocale(englishCountries);
+
+const countryOptions = Object.entries(
+  countries.getNames("en", { select: "official" })
+).map(([code, name]) => ({
+  value: code,
+  label: (
+    <div style={{ display: "flex", alignItems: "center" }}>
+      <span style={{ marginRight: 10 }}>{emojiFlag(code)}</span>
+      {name}
+    </div>
+  ),
+}));
+
+const customSelectStyles = {
+  control: (provided, state) => ({
+    ...provided,
+    backgroundColor: "#3f3f3f",
+    borderRadius: "0.5rem",
+    padding: "0.25rem",
+    fontSize: "0.875rem",
+    lineHeight: "1.25rem",
+    boxShadow: state.isFocused ? 0 : 0,
+    borderColor: state.isFocused ? "#ffc83b" : "#6b7280",
+    "&:hover": {
+      borderColor: state.isFocused ? "#ffc83b" : "#6b7280",
+    },
+  }),
+  menu: (provided) => ({
+    ...provided,
+    borderRadius: "0.5rem",
+    backgroundColor: "575757",
+  }),
+  input: (provided) => ({
+    ...provided,
+    color: "white",
+    "& input": {
+      font: "inherit",
+    },
+  }),
+  singleValue: (provided, state) => ({
+    ...provided,
+    color: "white",
+  }),
+  placeholder: (provided) => ({
+    ...provided,
+    color: "#d1d5db",
+  }),
+  option: (provided, state) => ({
+    ...provided,
+    cursor: "pointer",
+    backgroundColor: state.isFocused ? "#7d5e01" : "#575757",
+  }),
+};
+
 const Signup = ({ setMessage }) => {
   const navigate = useNavigate();
   return (
-    <div className="w-full flex justify-center py-8 ">
-      <div className="p-4 w-full max-w-sm bg-white rounded-lg border border-gray-200 shadow-md sm:p-6 md:p-8 dark:bg-surface-500 dark:border-surface-400 ">
+    <div className="w-full flex justify-center py-8">
+      <div className="p-4 w-full max-w-sm bg-white rounded-lg border border-gray-200 shadow-md sm:p-6 md:p-8 dark:bg-surface-500 dark:border-surface-400">
         <Formik
           initialValues={{
             username: "",
             email: "",
             password: "",
             confirmPassword: "",
+            country: "",
           }}
-          onSubmit={async (data, { resetForm }) => {
+          onSubmit={async (values, { setSubmitting, resetForm }) => {
             try {
-              const response = await signup(data);
+              const response = await signup(values);
               if (response) {
                 resetForm({});
                 setMessage({
@@ -30,7 +91,6 @@ const Signup = ({ setMessage }) => {
                   "signupEmail",
                   JSON.stringify(response.email)
                 );
-
                 navigate("/verify");
               }
             } catch (error) {
@@ -42,89 +102,120 @@ const Signup = ({ setMessage }) => {
           }}
           validationSchema={signupValidationSchema}
         >
-          {({ errors, handleChange, handleSubmit, values }) => {
-            return (
-              <form
-                className="space-y-6"
-                onSubmit={handleSubmit}
-                autoComplete="off"
-              >
-                <h5 className="text-xl font-medium text-gray-900 dark:text-white">
-                  Sign up to Fun Olympics 2024
-                </h5>
-                <div>
-                  <label
-                    htmlFor="username"
-                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                  >
-                    Full Name
-                  </label>
-                  <TextField
-                    type="text"
-                    name="username"
-                    placeholder="Enter your full name"
-                  />
-                </div>
-                <div>
-                  <label
-                    htmlFor="email"
-                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                  >
-                    Your Email
-                  </label>
-                  <TextField
-                    type="email"
-                    name="email"
-                    placeholder="name@company.com"
-                  />
-                </div>
-                <div>
-                  <label
-                    htmlFor="password"
-                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                  >
-                    Your Password
-                  </label>
-                  <TextField
-                    type="password"
-                    name="password"
-                    pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
-                    title="Must contain at least one  number and one uppercase and lowercase letter, and at least 8 or more characters"
-                    placeholder="••••••••"
-                  />
-                </div>
-                <div>
-                  <label
-                    htmlFor="confirm-password"
-                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                  >
-                    Confirm Password
-                  </label>
-                  <TextField
-                    type="password"
-                    name="confirmPassword"
-                    id="confirmPassword"
-                    placeholder="••••••••"
-                  />
-                </div>
-                <button
-                  type="submit"
-                  className="w-full text-white bg-primary-600 hover:bg-primary-600 focus:ring-4 focus:outline-none focus:ring-yellow-300 font-semibold rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-yellow-400 dark:focus:ring-yellow-500"
+          {({
+            errors,
+            handleChange,
+            handleSubmit,
+            values,
+            setFieldValue,
+            handleBlur,
+          }) => (
+            <form
+              className="space-y-6"
+              onSubmit={handleSubmit}
+              autoComplete="off"
+            >
+              <h5 className="text-xl font-medium text-gray-900 dark:text-white">
+                Sign up to Fun Olympics 2024
+              </h5>
+              <div>
+                <label
+                  htmlFor="username"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
                 >
-                  Sign up
-                </button>
-                <div className="text-sm font-medium text-gray-500 dark:text-gray-300">
-                  Already have an account?
-                  <Link
-                    to="/login"
-                    className="text-primary-600 hover:underline dark:text-primary-500"
-                  >
-                    &nbsp;Login
-                  </Link>
-                </div>
-              </form>
-            );
-          }}
+                  Full Name
+                </label>
+                <TextField
+                  type="text"
+                  name="username"
+                  placeholder="Enter your full name"
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="country"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                >
+                  Country
+                </label>
+                <Select
+                  options={countryOptions}
+                  isClearable
+                  isSearchable
+                  name="country"
+                  placeholder="Select your country"
+                  styles={customSelectStyles}
+                  onChange={(selectedOption) =>
+                    setFieldValue(
+                      "country",
+                      selectedOption ? selectedOption.value : ""
+                    )
+                  }
+                  onBlur={handleBlur}
+                  value={countryOptions.find(
+                    (option) => option.value === values.country
+                  )}
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="email"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                >
+                  Your Email
+                </label>
+                <TextField
+                  type="email"
+                  name="email"
+                  placeholder="name@company.com"
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="password"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                >
+                  Your Password
+                </label>
+                <TextField
+                  type="password"
+                  name="password"
+                  pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+                  title="Must contain at least one  number and one uppercase and lowercase letter, and at least 8 or more characters"
+                  placeholder="••••••••"
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="confirm-password"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                >
+                  Confirm Password
+                </label>
+                <TextField
+                  type="password"
+                  name="confirmPassword"
+                  id="confirmPassword"
+                  placeholder="••••••••"
+                />
+              </div>
+              <button
+                type="submit"
+                className="w-full text-white bg-primary-600 hover:bg-primary-600 focus:ring-4 focus:outline-none focus:ring-yellow-300 font-semibold rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-yellow-400 dark:focus:ring-yellow-500"
+              >
+                Sign up
+              </button>
+              <div className="text-sm font-medium text-gray-500 dark:text-gray-300">
+                Already have an account?
+                <Link
+                  to="/login"
+                  className="text-primary-600 hover:underline dark:text-primary-500"
+                >
+                  &nbsp;Login
+                </Link>
+              </div>
+            </form>
+          )}
         </Formik>
       </div>
     </div>
