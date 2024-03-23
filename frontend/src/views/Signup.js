cb30d4b38bfd49c6caa8b87046be75eb598a759b
NarayanSapkota <59175636+noxiousghost@users.noreply.github.com>
2024-03-23 12:58:48 +0545

    added select sport dropdown

--- frontend/src/views/Signup.js
+++ frontend/src/views/Signup.js
@@ -5,10 +5,127 @@ import Select from "react-select";
 import countries from "i18n-iso-countries";
 import englishCountries from "i18n-iso-countries/langs/en.json";
 import emojiFlag from "emoji-flag";
-
 import TextField from "../Components/TextField";
 import { signupValidationSchema } from "../FormValidation/validationSchema";
 import { signup } from "../services/loginSignup";
+import { IoMdFootball, IoMdBasketball, IoMdSnow } from "react-icons/io";
+import {
+  MdGolfCourse,
+  MdOutlineSportsTennis,
+  MdOutlineSportsBaseball,
+} from "react-icons/md";
+import { FaRunning, FaTableTennis, FaSkiing, FaSwimmer } from "react-icons/fa";
+import { GiArcher } from "react-icons/gi";
+
+const sportsOptions = [
+  {
+    value: "athletics",
+    label: (
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <FaRunning style={{ marginLeft: -10, marginRight: 10 }} /> Athletics
+      </div>
+    ),
+  },
+  {
+    value: "swimming",
+    label: (
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <FaSwimmer
+          style={{ marginLeft: -10, marginRight: 10, color: "blue" }}
+        />{" "}
+        Swimming
+      </div>
+    ),
+  },
+  {
+    value: "basketball",
+    label: (
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <IoMdBasketball
+          style={{ marginLeft: -10, marginRight: 10, color: "orange" }}
+        />{" "}
+        Basketball
+      </div>
+    ),
+  },
+  {
+    value: "football",
+    label: (
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <IoMdFootball
+          style={{ marginLeft: -10, marginRight: 10, color: "white" }}
+        />{" "}
+        Football
+      </div>
+    ),
+  },
+  {
+    value: "tableTennis",
+    label: (
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <FaTableTennis
+          style={{ marginLeft: -10, marginRight: 10, color: "purple" }}
+        />{" "}
+        Table Tennis
+      </div>
+    ),
+  },
+  {
+    value: "skiing",
+    label: (
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <FaSkiing
+          style={{ marginLeft: -10, marginRight: 10, color: "skyblue" }}
+        />{" "}
+        Skiing
+      </div>
+    ),
+  },
+  {
+    value: "tennis",
+    label: (
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <MdOutlineSportsTennis
+          style={{ marginLeft: -10, marginRight: 10, color: "yellowgreen" }}
+        />{" "}
+        Tennis
+      </div>
+    ),
+  },
+  {
+    value: "archery",
+    label: (
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <GiArcher
+          style={{ marginLeft: -10, marginRight: 10, color: "lightblue" }}
+        />{" "}
+        Archery
+      </div>
+    ),
+  },
+  {
+    value: "golf",
+    label: (
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <MdGolfCourse
+          style={{ marginLeft: -10, marginRight: 10, color: "darkgreen" }}
+        />{" "}
+        Golf
+      </div>
+    ),
+  },
+  {
+    value: "baseball",
+    label: (
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <MdOutlineSportsBaseball
+          style={{ marginLeft: -10, marginRight: 10, color: "red" }}
+        />{" "}
+        Baseball
+      </div>
+    ),
+  },
+];
 
 countries.registerLocale(englishCountries);
 
@@ -157,6 +274,44 @@ const Signup = ({ setMessage }) => {
                   )}
                 />
               </div>
+              <div>
+                <label
+                  htmlFor="favoriteSport"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                >
+                  Favorite Olympic Sport
+                </label>
+                <Select
+                  options={sportsOptions}
+                  isClearable
+                  isSearchable
+                  styles={customSelectStyles}
+                  name="favoriteSport"
+                  placeholder="Select your favorite sport"
+                  className="text-base"
+                  onChange={(selectedOption) =>
+                    setFieldValue(
+                      "favoriteSport",
+                      selectedOption ? selectedOption.value : ""
+                    )
+                  }
+                  onBlur={handleBlur}
+                  value={sportsOptions.find(
+                    (option) => option.value === values.favoriteSport
+                  )}
+                  getOptionLabel={(option) => (
+                    <div style={{ display: "flex", alignItems: "center" }}>
+                      <img
+                        src={option.icon}
+                        alt=""
+                        style={{ marginRight: 10 }}
+                      />
+                      {option.label}
+                    </div>
+                  )}
+                />
+              </div>
+
               <div>
                 <label
                   htmlFor="email"
