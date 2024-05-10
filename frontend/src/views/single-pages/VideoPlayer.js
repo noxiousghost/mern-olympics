df3dd66c9515dd18c95cd7146f168b12f1dd6473
NarayanSapkota <59175636+noxiousghost@users.noreply.github.com>
2024-05-10 22:28:22 +0545

    seperated comment section

--- frontend/src/views/single-pages/VideoPlayer.js
+++ frontend/src/views/single-pages/VideoPlayer.js
@@ -4,17 +4,17 @@ import { useNavigate, useParams } from "react-router-dom";
 import ResponsivePlayer from "../../Components/ResponsivePlayer";
 import VideoCard from "../../Components/VideoCard";
 import { getAll, getOne } from "../../services/video";
+import Comments from "../../Components/Comments";
 import Moment from "react-moment";
 import Loading from "../../Components/Loading";
 import NotExists from "../../Components/NotExists";
 import { AiOutlineStar } from "react-icons/ai";
 import { BsFillStarFill } from "react-icons/bs";
 import { addToFav, getOne as getOneUser } from "../../services/users";
-import { FaThumbsUp, FaThumbsDown, FaReply } from "react-icons/fa";
+
 const VideoPlayer = ({ user, setMessage }) => {
-  const [comment, setComment] = useState(""); // State to hold the comment
-  const [comments, setComments] = useState([]);
   const [video, setVideo] = useState({});
+  const [comment, setComment] = useState({});
   const [played, setPlayed] = useState(0);
   const { id } = useParams();
   const [isLoading, setIsLoading] = useState(true);
@@ -66,23 +66,6 @@ const VideoPlayer = ({ user, setMessage }) => {
       fetchVideo(id);
     }
   }, [id]);
-  // Function to handle comment submission
-  const handleCommentSubmit = () => {
-    // Add the new comment to the comments state
-    setComments([...comments, comment]);
-    // Clear the comment input field after submission
-    setComment("");
-  };
-
-  // useEffect(() => {
-  //   // title can be the fetched video title
-  //   return () => {
-  //     // save user's watch time
-
-  //     // console.log(played);
-  //     // console.log("Component unmount");
-  //   };
-  // }, [played]);
 
   const handleWatchTime = (state) => {
     // console.log(state);
@@ -164,53 +147,7 @@ const VideoPlayer = ({ user, setMessage }) => {
             </div>
           </>
         )}
-        <div className="my-3">
-          <span className="text-wheatt font-bold md:text-xl lg:text-2xl">
-            Comments
-          </span>
-          <hr className="mt-4 h-1 display-1" />
-          <div className="my-4">
-            {/* Form for submitting comments */}
-            <textarea
-              className="w-1/2 h-16 p-2.5 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 dark:bg-surface-400 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-              placeholder="Write your comment here..."
-              value={comment}
-              onChange={(e) => setComment(e.target.value)}
-            ></textarea>
-            <br />
-            <button
-              className="w-45 text-white font-medium rounded-lg text-sm px-3 py-2 text-center dark:bg-pink-700 dark:hover:bg-pink-800 dark:focus:ring-pink-900"
-              onClick={handleCommentSubmit}
-            >
-              Comment
-            </button>
-          </div>
-          {/* Display Comments */}
-          {/* Display each comment */}
-          {comments.map((comment, index) => (
-            <div key={index} className="px-6 py-3 my-2">
-              <p>
-                <img
-                  className="w-7 h-7 absolute -ml-8"
-                  alt="icon"
-                  src="/assets/profile-img/profile-1.png"
-                />
-                {comment}
-              </p>
-              <div className="mt-2 gap-x-3 flex items-center">
-                <button className="mr-2">
-                  <FaThumbsUp />
-                </button>
-                <button className="mr-2">
-                  <FaThumbsDown />
-                </button>
-                <a href="#" className="mr-2 text-blue-500 hover:underline">
-                  Reply
-                </a>
-              </div>
-            </div>
-          ))}
-        </div>
+        <Comments post={comment} />
         <div className="my-6">
           <span className="text-wheatt font-bold md:text-2xl lg:text-3xl">
             Related Videos
