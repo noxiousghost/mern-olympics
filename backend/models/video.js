34661ed386266cf623998cf09c6a7422f6792e52
NarayanSapkota <59175636+noxiousghost@users.noreply.github.com>
2024-05-17 22:08:02 +0545

    modified video model to store comments

--- backend/models/video.js
+++ backend/models/video.js
@@ -1,9 +1,15 @@
 import mongoose from "mongoose";
 
+const commentSchema = new mongoose.Schema({
+  text: { type: String, required: true },
+  postedBy: { type: mongoose.Schema.Types.ObjectId, ref: "users" },
+  postedAt: { type: Date, default: Date.now },
+});
+
 const videoSchema = new mongoose.Schema({
   title: { type: String, required: true },
   video_url: { type: String, required: true },
-  description:{type:String},
+  description: { type: String },
   views: { type: Number, default: 0 },
   category: {
     type: mongoose.Schema.Types.ObjectId,
@@ -12,6 +18,11 @@ const videoSchema = new mongoose.Schema({
   uploader: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "users",
+  },
+  comments: [commentSchema],
+  addedDate: {
+    type: Date,
+    default: Date.now,
   },
   addedDate: {
     type: Date,
