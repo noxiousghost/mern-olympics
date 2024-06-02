13312252b03b2677ab1c4a214fc1765a0ef0788d
NarayanSapkota <59175636+noxiousghost@users.noreply.github.com>
2024-06-02 22:40:09 +0545

    created separate comment component for common comment codes

--- frontend/src/interactions/NewsComment.js
+++ frontend/src/interactions/NewsComment.js
@@ -1,121 +1,24 @@
-import React, { useEffect, useState } from "react";
-import { FaThumbsUp, FaThumbsDown } from "react-icons/fa";
-import Moment from "react-moment";
+import React from "react";
 import { getComments, addComment } from "../services/news";
+import Comments from "./Comments";
 import { useNavigate } from "react-router-dom";
-import { getUser } from "../services/token"; // Importing the getUser function
 
-function Comments({ newsId, setMessage }) {
-  // Adding setMessage as a prop
-  const [comment, setComment] = useState("");
-  const [comments, setComments] = useState([]);
-  const [errorMessage, setErrorMessage] = useState("");
-  const navigate = useNavigate(); // Using navigate to redirect
+const NewsComment = ({ newsId, setMessage }) => {
+  const navigate = useNavigate();
 
-  const handleCommentSubmit = async () => {
-    const user = getUser(); // Getting the logged-in user information
-
-    if (!user) {
-      setMessage({
-        message: "Must login to add comments!",
-        className: "warning",
-      });
-      setErrorMessage("login to comment");
-      navigate("/login");
-      return;
-    }
-
-    if (comment.trim() !== "") {
-      try {
-        const newComment = await addComment(newsId, { text: comment });
-
-        // Adding the postedBy field with the current user's information to the new comment
-        newComment.postedBy = {
-          username: user.username,
-        };
-
-        setComments([newComment, ...comments]);
-        setComment("");
-        setErrorMessage("");
-      } catch (error) {
-        setErrorMessage(error.message || "Failed to post comment");
-      }
-    } else {
-      setErrorMessage("Please enter a comment before submitting.");
-    }
+  const redirectToLogin = () => {
+    navigate("/login");
   };
-
-  const fetchComments = async () => {
-    try {
-      const response = await getComments(newsId);
-      // Sort comments by postedAt in descending order
-      response.sort((a, b) => new Date(b.postedAt) - new Date(a.postedAt));
-      setComments(response);
-    } catch (error) {
-      console.error("Failed to fetch comments", error);
-    }
-  };
-
-  useEffect(() => {
-    fetchComments();
-  }, [newsId]);
 
   return (
-    <div className="my-3">
-      <span className="text-wheatt font-bold md:text-xl lg:text-2xl">
-        Comments
-      </span>
-      <hr className="mt-4 h-1 display-1" />
-      <div className="my-4">
-        {/* Form for submitting comments */}
-        <textarea
-          className="w-1/2 h-16 p-2.5 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 dark:bg-surface-400 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-          placeholder="Write your comment here..."
-          value={comment}
-          required
-          onChange={(e) => setComment(e.target.value)}
-        ></textarea>
-        {errorMessage && (
-          <p className="text-red-500 text-sm mt-1 -mb-2">{errorMessage}</p>
-        )}
-        <div className="mt-4">
-          <button
-            className="w-45 text-white font-medium rounded-lg text-sm px-3 py-2 text-center dark:bg-pink-700 dark:hover:bg-pink-800 dark:focus:ring-pink-900"
-            onClick={handleCommentSubmit}
-          >
-            Comment
-          </button>
-        </div>
-      </div>
-
-      {comments.map((comment, index) => (
-        <div key={index} className="px-6 py-3 my-2">
-          <p className="relative">
-            <img
-              className="w-7 h-7 absolute -ml-8"
-              alt="icon"
-              src="/assets/profile-img/profile-1.png"
-            />
-            {comment.postedBy && comment.postedBy.username}: {comment.text}
-          </p>
-          <p>
-            <Moment fromNow>{comment.postedAt}</Moment>
-          </p>
-          <div className="mt-2 gap-x-3 flex items-center">
-            <button className="mr-2">
-              <FaThumbsUp />
-            </button>
-            <button className="mr-2">
-              <FaThumbsDown />
-            </button>
-            <a href="#" className="mr-2 text-blue-500 hover:underline">
-              Reply
-            </a>
-          </div>
-        </div>
-      ))}
-    </div>
+    <Comments
+      itemId={newsId}
+      getComments={getComments}
+      addComment={addComment}
+      setMessage={setMessage}
+      redirectToLogin={redirectToLogin}
+    />
   );
-}
+};
 
-export default Comments;
+export default NewsComment;
