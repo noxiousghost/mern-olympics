41ddbe8e02d787a08a6a8e9e80cb53583ba5188f
NarayanSapkota <59175636+noxiousghost@users.noreply.github.com>
2024-08-28 10:42:32 +0545

    add file size limit and file type validation in multer

--- backend/utils/multer.js
+++ backend/utils/multer.js
@@ -1,31 +1,48 @@
 import multer from "multer";
-import {v4} from "uuid";
+import { v4 } from "uuid";
 import fs from "fs";
+
+const fileFilter = (req, file, cb) => {
+  const allowedTypes = /jpeg|jpg|png|gif|mp4|mov|avi/; // regex for checking file format
+  const mimeType = allowedTypes.test(file.mimetype); // testing the file type with above regex
+  const extname = allowedTypes.test(
+    path.extname(file.originalname).toLowerCase()
+  );
+
+  if (mimeType && extname) {
+    return cb(null, true);
+  } else {
+    cb(new Error("Invalid file type. Only images and videos are allowed."));
+  }
+};
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     const fileType = req.fileType;
-    let path;
+    let uploadPath;
     if (fileType) {
-      path = `public/uploads/${fileType}`;
+      uploadPath = `public/uploads/${fileType}`;
     } else {
-      path = `public/uploads/random`;
+      uploadPath = `public/uploads/random`;
     }
-    fs.mkdirSync(path, { recursive: true });
-    cb(null, path);
+    fs.mkdirSync(uploadPath, { recursive: true });
+    cb(null, uploadPath);
   },
   filename(req, file, cb) {
-    // ${v4()}-
-    const newFileName = `${file.originalname.trim()}`;
+    const newFileName = `${uuidv4()}-${file.originalname.trim()}`;
     cb(null, newFileName);
   },
 });
 const uploadImage = multer({
   storage: storage,
+  limits: { fileSize: 5 * 1024 * 1024 }, // 5 MB limit for images
+  fileFilter: fileFilter,
 }).single("image");
 
 const uploadVideo = multer({
   storage: storage,
+  limits: { fileSize: 100 * 1024 * 1024 }, // 100 MB limit for videos
+  fileFilter: fileFilter,
 }).single("video");
 
 export { uploadImage, uploadVideo };
